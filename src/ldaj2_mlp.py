42ee0dc69dceedfd3d3f553213c4eface9b82798
YWQAQWY <smsmun.yanwq@outlook.com>
2026-05-08 23:43:03 +0800

    v0.2

---
+++ src/ldaj2_mlp.py
@@ -0,0 +1,97 @@
+from sklearn.datasets import fetch_openml
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+mnist = fetch_openml('mnist_784', version=1)
+
+x = mnist.data
+y = mnist.target.astype(int)
+
+scaler = StandardScaler()
+x_scaled = scaler.fit_transform(x)
+
+x_train, x_temp, y_train, y_temp = train_test_split(
+    x_scaled,
+    y,
+    test_size=0.3,
+    random_state=42,
+)
+
+x_val, x_test, y_val, y_test = train_test_split(
+    x_temp,
+    y_temp,
+    test_size=0.5,
+    random_state=42
+)
+
+dimension = 9
+lda = LinearDiscriminantAnalysis(n_components=dimension)
+x_train_lda = lda.fit_transform(x_train, y_train)
+x_val_lda = lda.transform(x_val)
+x_test_lda = lda.transform(x_test)
+
+mlp = MLPClassifier(
+    hidden_layer_sizes=(128,64),
+    activation='relu',
+    max_iter=50,
+    random_state=42
+)
+
+mlp.fit(x_train_lda, y_train)
+
+y_pred = mlp.predict(x_test_lda)
+acc = accuracy_score(y_test, y_pred)
+print(f"Test Accuracy: {acc:.4f}")
+
+cm = confusion_matrix(y_test, y_pred)
+
+
+print("\nClassification Report:")
+print(
+    classification_report(
+        y_test,
+        y_pred
+    )
+)
+
+# Visualization
+plt.figure(figsize=(10,8))
+sns.heatmap(
+    cm,
+    annot=True,
+    fmt='d',
+    cmap='Greens'
+)
+plt.title("LDA + MLP Confusion Matrix")
+plt.xlabel("Predicted")
+plt.ylabel("True")
+plt.savefig("/home/yanwq/pattern_recognition/results/figures/mnist_lda_mlp_confusion_matrix.png")
+plt.close()
+
+plt.figure(figsize=(8,6))
+scatter = plt.scatter(
+    x_test_lda[:,0],
+    x_test_lda[:,1],
+    c=y_test,
+    s=10
+)
+plt.title("MNIST LDA Visualization")
+plt.xlabel("LD1")
+plt.ylabel("LD2")
+plt.colorbar(scatter)
+plt.savefig("/home/yanwq/pattern_recognition/results/figures/mnist_lda_visualization.png")
+plt.close()
+
+#========================================================================
+# VALIDATION
+#========================================================================
+y_val_pred = mlp.predict(x_val_lda)
+val_acc = accuracy_score(y_val, y_val_pred)
+print(f"Validation Accuracy: {val_acc:.4f}")
+
+
--- src/pca_svm.py
+++ src/pca_svm.py
@@ -48,6 +48,7 @@ cm = confusion_matrix(y_test, y_pred)
 print("Confusion Matrix:")
 print(cm)
 
+""" Visualization"""
 sns.heatmap(cm, annot=True, fmt='d', cmap='Blues')
 plt.title("PCA + SVM Confusion Matrix")
 plt.xlabel("Predicted Label")
@@ -59,6 +60,7 @@ report = classification_report(y_test, y_pred)
 print("Classification Report:")
 print(report)
 
+"""SVM Visualization"""
 plt.figure(figsize=(8, 6))
 scatter = plt.scatter(
     x_test_pca[:, 0],
@@ -72,58 +74,44 @@ plt.legend(*scatter.legend_elements(), title="Classes")
 plt.savefig("/home/yanwq/pattern_recognition/results/figures/pca_visualization.png")
 plt.close()
 
-"""SVM Visualization"""
-# 生成二维平面范围
 x_min = x_train_pca[:, 0].min() - 1
 x_max = x_train_pca[:, 0].max() + 1
-
 y_min = x_train_pca[:, 1].min() - 1
 y_max = x_train_pca[:, 1].max() + 1
 
-
-# 生成网格点
 xx, yy = np.meshgrid(
     np.arange(x_min, x_max, 0.01),
     np.arange(y_min, y_max, 0.01)
 )
 
-
-# SVM预测整个平面
 Z = svm.predict(
     np.c_[xx.ravel(), yy.ravel()]
 )
-
-# 变回网格形状
 Z = Z.reshape(xx.shape)
 
-
-# 绘图
 plt.figure(figsize=(8, 6))
-
-# 分类区域
 plt.contourf(
     xx,
     yy,
     Z,
     alpha=0.3
 )
-
-# 样本点
 scatter = plt.scatter(
     x_test_pca[:, 0],
     x_test_pca[:, 1],
     c=y_test,
     edgecolors='k'
 )
-
 plt.title("SVM Decision Boundary")
-
 plt.xlabel("Principal Component 1")
-
 plt.ylabel("Principal Component 2")
-
 plt.legend(*scatter.legend_elements(), title="Classes")
-
 plt.savefig("/home/yanwq/pattern_recognition/results/figures/svm_decision_boundary.png")
-
 plt.close()
+
+#========================================================================
+# VALIDATION
+#========================================================================
+y_val_pred = svm.predict(x_val_pca)
+val_acc = accuracy_score(y_val, y_val_pred)
+print(f"Validation Accuracy: {val_acc:.4f}")
